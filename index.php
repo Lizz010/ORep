c5b867b6ffd583bf061da7b6daa4baa8c211f27c
lizz <lizzp0110@gmail.com>
2023-06-01 23:21:07 -0500

    index

---
+++ index.php
@@ -0,0 +1,296 @@
+ <!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no"><!-- esta etiqueta permite configurar cómo debe interpretar una página el navegador web para móviles -->
+	<title>Liz Pemberthy</title>
+</head>
+
+<body>
+	<a id="whatsapp" 
+	href="https://api.whatsapp.com/send?phone=573046252676"><img src="imgs/whatsapp.png" alt=""></a>
+
+	<header>
+		<div class="menu">
+			<div class="contenedor">
+				<p class="logo">LIZ PEMBERTHY</p>
+				<img class="menu-icon" src="imgs/menu.png">
+				<nav>
+					<ul>
+						<li><a href="#sobre-mi">Sobre Mi</a></li>
+						<li><a href="#servicios">Servicios</a></li>
+						<li><a href="#mis-trabajos">Mis Trabajos</a></li>
+						<li><a href="#porque-trabajar">¿Por qué Trabajar Conmigo?</a></li>
+						<li><a href="#blog">Blog</a></li>
+						<li><a href="#contacto">Contacto</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+
+		<div class="contenedor" id="contenedor-titulo-flex">
+			<div class="contenedor-titulo">
+				<h1>Liz Pemberthy</h1>
+				<h2>Diseñadora & Desarrolladora Web</h2>
+				<a href="">Ver Mis Trabajos</a>
+			</div>
+		</div>
+	</header>
+
+	<main>
+		<!--SECCIÓN SOBRE MI-->
+		<section id="sobre-mi">
+			<div class="contenedor">
+				<h3>Sobre Mi</h3>
+				<div class="contenedor-sobremi">
+					<div class="computadoras">
+						<img src="imgs/computadoras.png" alt="Computadoras">
+					</div>
+					<div class="texto">
+							<p>Mi nombre es Liz Pemberthy y soy diseñadora de páginas web. Tengo la experiencia en desarrollo de sitios web para pequeñas y medianas empresas, hasta tiendas en linea con carrito de compras.</p>
+
+							<p>Me aseguro de utilizar siempre las últimas tecnologías de desarrollo web: HTML5, CSS3, jQUERY, Wordpress. Si necesitas una página web moderna, funcional, que se adapte a todos los diferentes tipos de pantalla, te aseguro que llegaste al lugar correcto.</p>
+
+							<p>Puedes ponerte en contacto conmigo a través de Facebook, Linkedin o completando el formulario de contacto que se encuentra al final de mi sitio web.</p>
+							<a href="">Ver Mis Trabajos</a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!--SECCIÓN SERVICIOS-->
+		<section id="servicios">
+			<div class="contenedor">
+				<h3>Servicios</h3>
+				<div class="contenedor-servicios">
+					<div class="servicio verde">
+						<h4>Diseño Web</h4>
+						<p>Páginas web atractivas y funcionales con el objetivo de satisfacer todas tus expectativas. La importancia del diseño es fundamental para tener éxito y atraer a tus visitantes. También lo es la velocidad de carga del sitio web, deben ser rápidos para que el visitante tenga la mejor experiencia.</p>
+						<img class="icon" src="imgs/diseñoweb.png" alt="">
+						<img class="onda" src="imgs/onda.png" alt="">
+					</div>
+
+					<div class="servicio rosa">
+						<h4>Responsive Design</h4>
+						<p>Hoy en dia las páginas web se ven en multitud de dispositivos como tablets, celulares, libros electrónicos, notebooks, PCs, etc. La tecnologia móvil es el presente y el futuro. Para eso es importante y me aseguro de que tu sitio web sea compatible con todos los tamaños de pantalla de esta época moderna.</p>
+						<img class="icon" src="imgs/celular.png" alt="">
+						<img class="onda" src="imgs/onda.png" alt="">
+					</div>
+
+					<div class="servicio verde">
+						<h4>SEO</h4>
+						<p>Al crear sitios web, tengo muy en cuenta la optimización de motores de busqueda(SEO). Llevo a cabo practicas de SEO estándar en cada página del sitio web, lo que permite que tu sitio tenga una clasificación más alta en los resultados de Google y así generar mas tráfico a la web.</p>
+						<img class="icon" src="imgs/seo.png" alt="">
+						<img class="onda" src="imgs/onda.png" alt="">
+					</div>
+
+					<div class="servicio rosa">
+						<h4>Autoadministrable</h4>
+						<p>Es importante que puedas administrar tu página web tu mismo, desde cualquier lugar y en cualquier momento. Desarrollo sistemas de gestión de contenido que te permiten ver los datos de tu página web y editar el contenido que quieras, lo que te permite mantenerte actualizado.</p>
+						<img class="icon" src="imgs/wordpress.png" alt="">
+						<img class="onda" src="imgs/onda.png" alt="">
+					</div>
+
+					<div class="servicio verde">
+						<h4>Mantenimiento</h4>
+						<p>Puedes contactarme si necesitas actualizar tu página web, agregar nuevas secciones o cualquier cambio que necesites. Esto es perfecto para los sitios web de pequeñas empresas, donde no se necesita un diseñador de páginas web a tiempo completo, sino solo en distintas ocasiones.</p>
+						<img class="icon" src="imgs/mante.png" alt="">
+						<img class="onda" src="imgs/onda.png" alt="">
+					</div>
+
+					<div class="servicio rosa">
+						<h4>Marketing Digital</h4>
+						<p>Utilizo las herramientas mas importantes como Google Adwards y Google Analytics, para que tu negocio aparezca en los resultados de búsqueda de Google justo cuando alguien te necesita y analizar como los usuarios interactúan con tu página web.</p>
+						<img class="icon" src="imgs/marketing.png" alt="">
+						<img class="onda" src="imgs/onda.png" alt="">
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!--SECCIÓN MIS TRABAJOS-->  
+		<section id="mis-trabajos">	
+			<div class="contenedor">
+				<h3>Mis Trabajos</h3>
+				    <div class="owl-carousel owl-theme">
+	    				<div class="item"><img src="imgs/cookiee.png" alt=""><p>Imagen1</p></div>
+	    				<div class="item"><img src="imgs/cookiee1.png" alt=""><p>Imagen2</p></div>
+	    				<div class="item"><img src="imgs/cookiee2.png" alt=""><p>Imagen3</p></div>
+	    				<div class="item"><img src="imgs/cookiee3.png" alt=""><p>Imagen4</p></div>
+	    				<div class="item"><img src="imgs/cookiee4.png" alt=""><p>Imagen5</p></div>
+					</div> 
+			</div>
+		</section>
+
+		<!--SECCIÓN PORQUE TRABAJAR-->  
+		<section id="porque-trabajar">
+			<div class="contenedor">
+				<h3>¿Por que Trabajar Conmigo?</h3>
+				<div class="contenedor-trabajar">
+					<div class="item">
+						<ul>
+							<li><img src="imgs/velocidad.png" alt=""></li>
+							<li>Velocidad</li>
+							<li>Me aseguro de que tu web cargue entre 2 y 4 segundos(depende de la cantidad de contenido del sitio). Si una web tarda mas de 5 segundos en cargar, el 70% de los usuarios lo cierra y no vuelve a entrar mas.</li>
+						</ul>
+					</div>
+					<div class="item">
+						<ul>
+							<li><img src="imgs/mantenimiento.png" alt=""></li>
+							<li>Asesoramiento</li>
+							<li>Mantengo una comunicación constante con mi cliente, para enseñarte a modificar contenido mediante el panel de Wordpress o para cualquier duda/cambio que quiera hacer en su sitio.</li>
+						</ul>
+					</div>
+					<div class="item">
+						<ul>
+							<li><img src="imgs/mobile.png" alt=""></li>
+							<li>Mobile Priority</li>
+							<li>Cada vez mas personas navegan desde sus celulares en cualquier parte del mundo. Me aseguro de que tu web esté optimizada para cualquier tipo de dispositivo movil.</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!--SECCIÓN BLOG-->  
+		<section id="blog">
+			<div class="contenedor">
+				<h3>Blog</h3>
+				<div class="contenedor-publicaciones">
+					<div class="publicacion">
+						<img src="imgs/blog.jpg" alt="">
+						<div class="contenido">
+							<h4>No alcanza con tener solamente una página web</h4>
+							<p>¿Quierés aprender cuales son las herramientas que usan las mejores marcas para generar tráfico?</p>
+							<a href="">Leer mas →</a>
+						</div>
+					</div>
+					<div class="publicacion">
+						<img src="imgs/blog1.jpg" alt="">
+						<div class="contenido">
+							<h4>¿Qué es el remarketing y como utilizarlo?</h4>
+							<p>El remarketing es una de las técnicas mas utilizadas por las mejores compañías!
+							¿Quieres saber de que se trata?</p>
+							<a href="">Leer mas →</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!--SECCIÓN QUE ESPERAS-->  
+		<section id="que-esperas">
+			<div class="contenedor">
+				<h3>¿Qué esperas para tener tu página web?</h3>
+				<a href="">CHARLEMOS</a>
+			</div>
+		</section>
+
+		<!--SECCIÓN CONTACTO-->  
+		<section id="contacto">
+			<?php
+      			if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+      			<h4>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h4>
+      		<?php
+
+      			} else{
+      		?>
+			<div class="contenedor">
+				<h3>Contacto</h3>
+				<form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+					<input type="text" placeholder="Nombre" name="nombre" required="">
+					<input type="email" placeholder="Email" name="email" required="">
+					<textarea placeholder="Mensaje" name="mensaje" required=""></textarea>
+					<input type="submit" value="Enviar Mensaje">
+				</form>
+			<?php } ?>
+
+				<div class="contacto-info">
+					<div class="mail">
+						<img src="imgs/mail-icon.png" alt="">lizz0110@hotmail.com</div>
+					<div class="whatsapp">
+						<img src="imgs/whatsapp-icon.png" alt="">+573046252676</div>
+				</div>
+			</div>
+		</section>
+
+	</main>
+
+	<footer>
+		<div class="contenedor">
+			<div class="redes">
+				<a href="#"><img src="imgs/facebook.png" alt="facebook"></a>
+				<a href="#"><img src="imgs/medium.png" alt=""></a>
+				<a href="#"><img src="imgs/github.png" alt=""></a>
+				<a href="#"><img src="imgs/linkedin.png" alt=""></a>
+			</div>
+			<div class="parrafo">
+				<p>2019 Liz Pemberthy | Diseñadora Web Barranquilla-Colombia. Todos los derechos reservados.</p>
+			</div>
+		</div>
+	</footer>
+
+    <!--SCRIPT DE JQUERY EN ARCHIVO LOCAL-->
+	<!--<script src="js/jquery-3.4.1.min.js"></script>-->
+	<!--SCRIPT DE JQUERY DESDE UN CDN(CONTENT DELIVERY NETWORK) CDN DE JQUERY-->
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" 
+	integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script src="owl/owl.carousel.min.js"></script>
+	<script>
+		$('.owl-carousel').owlCarousel({
+    			loop:true,
+    			margin:10,
+    			nav:true,
+    			dots:false,
+    			responsive:{
+        			0:{
+            			items:1
+        			},
+        			600:{
+            			items:3
+        			},
+        			1000:{
+            			items:4
+        			}
+    			}
+			})
+	</script>
+
+	<!--SCRIPT DE LA BARRA DE NAVEGACIÓN-->
+	<script>
+		$(document).ready(function(){
+			$(window).scroll(function(){ 
+				scroll = $(window).scrollTop();
+				if (scroll > 100) {
+					$('.menu').css({"position":"fixed"});
+					$('.menu').css({"width":"100%"});
+					$('.menu').css({"top":"0"});
+					$('.menu').css({"background":"#FFFFFF"});
+					$('.menu a').css({"color":"#000000"});
+					$('.logo').css({"color":"#000000"});
+					$('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.09) 0px 1px 1px"});
+					$('.menu').css({"z-index":"100"});
+				} else {
+					$('.menu').css({"position":"relative"});
+					$('.menu').css({"background":"transparent"});
+					$('.menu').css({"box-shadow":"0 0 0"});
+					$('.menu a').css({"color":"#FFFFFF"});
+					$('.logo').css({"color":"#FFFFFF"});
+				} 
+			})
+
+			$('.menu-icon').click(function(){   	/*FUNCION DEL MENU DESPEGABLE EN MOVIL*/
+				$('header nav').slideToggle();
+
+			})       
+
+		})  
+
+	</script>
+
+</body>
+</html>
